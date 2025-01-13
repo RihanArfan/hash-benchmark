67ea4d20cd69262475035d10a158dc58a8857b83
Rihan Arfan <me@file.properties>
2025-01-13 07:31:00 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# hash benchmark
+
+Benchmarking different hashing algorithms.
+
+Uses [mitata](https://github.com/evanwashere/mitata) for benchmarking.
---
+++ benchmark.mjs
@@ -0,0 +1,55 @@
+import { createHash } from 'crypto';
+import { hash as blake3hash } from 'blake3-wasm'
+import { run, bench, barplot, summary, do_not_optimize } from 'mitata';
+import { createStorage } from 'unstorage';
+import fsDriver from 'unstorage/drivers/fs'
+
+// run with node --expose-gc disk-benchmark.mjs
+
+// Test data
+const srcStorage = createStorage({
+  driver: fsDriver({
+    base: '../ui/docs/dist',
+    ignore: ['.DS_Store']
+  }),
+})
+
+const fileKeys = await srcStorage.getKeys()
+const filesToDeploy = fileKeys.filter(fileKey => {
+  if (fileKey.startsWith('.wrangler:')) return false
+  if (fileKey.startsWith('node_modules:')) return false
+  if (fileKey === 'wrangler.toml') return false
+  if (fileKey === '.dev.vars') return false
+  if (fileKey.startsWith('database:migrations:')) return false
+  return true
+})
+
+// Hashing functions
+const hashMethods = {
+  md5: (data) => createHash('md5').update(data).digest('hex'),
+  sha1: (data) => createHash('sha1').update(data).digest('hex'),
+  sha256: (data) => createHash('sha256').update(data).digest('hex'),
+  sha512: (data) => createHash('sha512').update(data).digest('hex'),
+  blake2b512: (data) => createHash('blake2b512').update(data).digest('hex'),
+  blake2s256: (data) => createHash('blake2s256').update(data).digest('hex'),
+  blake3: (data) => blake3hash(data).toString('hex'),
+}
+
+barplot(() => {
+  summary(() => {
+    Object.entries(hashMethods).forEach(([name, fn]) => {
+      // bench(`${name}`, () => do_not_optimize(async () => {
+      bench(`${name}`, function* (state) {
+        yield async () => {
+          for (const fileKey of filesToDeploy) {
+            const data = await srcStorage.getItemRaw(fileKey)
+            const fileContentBase64 = data.toString('base64')
+            const hash = fn(fileContentBase64)
+          }
+        }
+      })
+    })
+  })
+})
+
+await run();
